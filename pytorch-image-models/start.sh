a1a4e3ec80603f7f74537899f5bcafd96a571ea8
GuangHui Xu <xuguanghui@gpu024.scut-smil.cn>
2021-01-27 16:52:02 +0800

    modify output dir

---
+++ pytorch-image-models/start.sh
@@ -0,0 +1,2 @@
+bash run_imagenet.sh 2 /dockerdata/imagenet 120 512 /apdcephfs/share_1318108/huberyniu/inas/mobilenetv3_large
+bash run_imagenet.sh 2 /dockerdata/imagenet 140 512 /apdcephfs/share_1318108/huberyniu/inas/mobilenetv3_large
